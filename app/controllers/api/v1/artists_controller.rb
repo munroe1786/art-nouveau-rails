76b801616fd80fc7a0d6ddefb4f1309c39465626
munroe1786 <era3munroe@gmail.com>
2020-05-27 15:47:01 -0500

    build out show and destroy artist actions / write before action set artist method

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ArtistsController < ApplicationController
+    before_action :set_artist, only: [:show, :update, :destroy]
 
     def index
         @artists = Artist.all
@@ -15,14 +16,20 @@ class Api::V1::ArtistsController < ApplicationController
     end
 
     def show
-
+        @artist = Artist.find(params[:id])
+        render json: @artist
     end
 
     def destroy
-
+        @artist = Artist.find(params[:id])
+        @artist.destroy
     end
     
     private
+
+    def set_artist
+        @artist = Artist.find(params[:id])
+    end
 
     def artist_params
         params.require(:artist).permit(:name, :date_of_birth, :date_of_death, :years_active, :nationality)
