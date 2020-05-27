b9da9f780d3e1d5086d6d07543e3374d4beefdf7
munroe1786 <era3munroe@gmail.com>
2020-05-27 15:19:33 -0500

    sketch out Artist controller actions and add strong params

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::ArtistsController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+    
+    private
+
+    def artist_params
+        params.require(:artist).permit(:name, :date_of_birth, :date_of_death, :years_active, :nationality)
+    end
+
+
 end
