03a24a99341b3a33258d5d95921c0340beefc02d
munroe1786 <era3munroe@gmail.com>
2020-05-27 15:52:03 -0500

    copy artist controller actions to work controller actions and make changes to apply to works

--- app/controllers/api/v1/works_controller.rb
+++ app/controllers/api/v1/works_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::WorksController < ApplicationController
+    before_action :set_work, only: [:show, :update, :destroy]
+
+    def index
+        @works = Work.all
+        render json: @works
+    end
+
+    def create
+        @work = Work.new(work_params)
+        if @work.save
+            render json: @work
+        else
+            render json: {error: 'Error creating work'}
+        end
+    end
+
+    def show
+        @work = work.find(params[:id])
+        render json: @work
+    end
+
+    def destroy
+        @work = work.find(params[:id])
+        @work.destroy
+    end
+    
+    private
+
+    def set_work
+        @work = work.find(params[:id])
+    end
+
+    def work_params
+        params.require(:work).permit(:artist_id, :image_url, :title, :date, :media, :description)
+    end
+
+
 end
