59d5b44fe80024fc4f5eebfca6c4e1f3931a9d69
munroe1786 <era3munroe@gmail.com>
2020-05-27 11:32:37 -0500

    add validations to models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,4 @@
 class Artist < ApplicationRecord
     has_many :works
+    validates :name, :date_of_birth, :date_of_death, :active_years, :nationality, presence: true
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,4 @@
 class Work < ApplicationRecord
     belongs_to :artist
+    validates :title, :date, :media, :description, presence: true
 end
