4694e9d506302432531eef340e99074d07c15e0f
munroe1786 <era3munroe@gmail.com>
2020-05-27 11:24:01 -0500

    use resource generator to create Work resource

---
+++ db/migrate/20200527162247_create_works.rb
@@ -0,0 +1,13 @@
+class CreateWorks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :works do |t|
+      t.integer :artist_id
+      t.string :title
+      t.integer :date
+      t.string :media
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
